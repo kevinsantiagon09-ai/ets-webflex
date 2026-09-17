bd9b806a7b61a1fcb42d104ff16583221d951878
Kevin Santiago <kevinsantiagon09@gmail.com>
2026-09-17 10:32:32 -0500

    creacion de tabla banners

---
+++ database/migrations/2026_09_17_152704_create_banners_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('banners', function (Blueprint $table) {
+            $table->id();
+
+            $table->uuid('uuid')->unique();
+
+            $table->foreignId('user_id')
+                ->constrained('users')
+                ->cascadeOnUpdate()
+                ->cascadeOnDelete();
+
+            $table->foreignId('estado_id')
+                ->constrained('estados')
+                ->cascadeOnUpdate()
+                ->restrictOnDelete();
+
+            $table->string('image_path', 255);
+
+            $table->string('titulo', 150);
+
+            $table->timestamps();
+
+            $table->softDeletes();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('banners');
+    }
+};
